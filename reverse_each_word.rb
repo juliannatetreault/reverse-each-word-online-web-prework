1050d060c654a42c3fb14ed2cac79d5c68eb0157
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-13 10:20:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,6 @@ def reverse_each_word(string)
 end
 
 def reverse_each_word(string)
-  string.split.collect{|w| w.reverse}.join(" ")
+  reverse = string.split.collect{|w| w.reverse}
+  reverse.join(" ")
 end
